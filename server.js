2ecbfb312121cb79dbefbb23ebe5710d7643b355
Fazlul Alam <fazlulalam619@gmail.com>
2025-04-12 23:19:28 +0600

    add index.js file

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.get("/", (req, res) => res.send("API Running..."));
 app.use('/api/auth', authRoutes); 
 app.use("/api/transactions", transactionRoutes); 
 
-
+module.exports = app;
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
