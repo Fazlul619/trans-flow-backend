a6a681db6f214a20de1d78158e76742d0fd4d692
Fazlul Alam <fazlulalam619@gmail.com>
2025-04-12 23:34:40 +0600

    server.js

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.use('/api/auth', authRoutes);
 app.use("/api/transactions", transactionRoutes); 
 
 module.exports = app;
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
