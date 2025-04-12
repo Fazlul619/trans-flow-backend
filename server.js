eb297ea0092abe4c2d4fdfd65824e5e89344db05
Fazlul Alam <fazlulalam619@gmail.com>
2025-04-13 00:24:16 +0600

    run locally

--- server.js
+++ server.js
@@ -17,5 +17,5 @@ app.use('/api/auth', authRoutes);
 app.use("/api/transactions", transactionRoutes); 
 
 module.exports = app;
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
